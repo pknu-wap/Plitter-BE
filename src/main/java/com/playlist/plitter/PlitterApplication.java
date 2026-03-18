bf1ef78c3701b3f6793d918a0da33642ca340c84
김도현 KimDohyun <163822282+pu2rile@users.noreply.github.com>
2026-03-18 16:52:18 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/playlist/plitter/PlitterApplication.java
@@ -0,0 +1,13 @@
+package com.playlist.plitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlitterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playlist/plitter/PlitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playlist.plitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
