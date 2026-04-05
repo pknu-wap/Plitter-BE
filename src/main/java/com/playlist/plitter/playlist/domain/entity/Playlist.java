10af6feeda2728f7fc94746e9fe90aee36ca0f13
MinseoKim01 <202513341@pukyong.ac.kr>
2026-04-06 02:13:45 +0900

    feat: Playlist 엔티티 추가

---
+++ src/main/java/com/playlist/plitter/playlist/domain/entity/Playlist.java
@@ -0,0 +1,42 @@
+package com.playlist.plitter.playlist.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_user_id", nullable = false)
+    private Users owner;
+
+    @Column(name = "short_id", nullable = false, length = 20)
+    private String short_id;
+
+    @Column(name = "recommendation_count", nullable = false)
+    private Integer recommendation_count;
+
+    @Column(name = "character_version", nullable = false)
+    private Integer character_version;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
