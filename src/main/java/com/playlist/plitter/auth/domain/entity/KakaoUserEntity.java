7a065b24dd6191e44f966296daeffaf4de2eb92a
JunYoung <pde4735@naver.com>
2026-04-09 20:29:01 +0900

    feat: KakaoUser Entity

---
+++ src/main/java/com/playlist/plitter/auth/domain/entity/KakaoUserEntity.java
@@ -0,0 +1,41 @@
+package com.playlist.plitter.auth.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class KakaoUserEntity {
+
+    @Builder
+    public KakaoUserEntity(String kakaoId, String nickname) {
+        this.kakaoId = kakaoId;
+        this.nickname = nickname;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "kakao_id", nullable = false, length = 100)
+    private String kakaoId;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
