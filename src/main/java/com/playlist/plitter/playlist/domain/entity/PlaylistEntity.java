3a14199c3caf5417239671b14cecade8f498c47c
MinseoKim01 <202513341@pukyong.ac.kr>
2026-04-10 03:16:43 +0900

    refactor: 리뷰 반영 - PlaylistEntity 빌더 패턴 및 기본값 설정 적용
    - 필수 필드만 받는 생성자에 @Builder 적용
    - recommendationCount 기본값 설정
    - shortId 필드에 unique 제약조건 추가

--- src/main/java/com/playlist/plitter/playlist/domain/entity/PlaylistEntity.java
+++ src/main/java/com/playlist/plitter/playlist/domain/entity/PlaylistEntity.java
@@ -3,6 +3,7 @@ package com.playlist.plitter.playlist.domain.entity;
 import com.playlist.plitter.auth.domain.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
+@SuperBuilder
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "playlist")
 public class PlaylistEntity {
@@ -24,11 +26,12 @@ public class PlaylistEntity {
     @JoinColumn(name = "owner_user_id", nullable = false, unique = true)
     private UserEntity owner;
 
-    @Column(name = "short_id", nullable = false, length = 20)
+    @Column(name = "short_id", nullable = false, length = 20, unique = true)
     private String shortId;
 
+    @Builder.Default
     @Column(name = "recommendation_count", nullable = false)
-    private int recommendationCount;
+    private int recommendationCount = 0;
 
     @Column(name = "character_version", nullable = false)
     private int characterVersion;
@@ -40,4 +43,11 @@ public class PlaylistEntity {
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    @Builder
+    public PlaylistEntity(UserEntity owner, String shortId, int characterVersion) {
+        this.owner = owner;
+        this.shortId = shortId;
+        this.characterVersion = characterVersion;
+    }
 }
