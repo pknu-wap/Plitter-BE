d514c80d2dbf0d2e37bd31a1873a156bac1e6e34
MinseoKim01 <202513341@pukyong.ac.kr>
2026-04-12 11:38:52 +0900

    refactor: 리뷰반영 - 플레이리스트 생성 로직을 컨트롤러에서 서비스로 이동
    - 컨트롤러의 유저 조회, 중복 검사, shortId 생성, 엔티티 생성 로직을 서비스로 이동시킴
    - 컨트롤러는 서비스 호출과 응답 반환만 하도록 단순화
    - 생성 직후에는 캐릭터가 없으니 캐릭터 버전의 초기값을 1 > 0 으로 변경

--- src/main/java/com/playlist/plitter/playlist/application/PlaylistService.java
+++ src/main/java/com/playlist/plitter/playlist/application/PlaylistService.java
@@ -3,23 +3,41 @@ package com.playlist.plitter.playlist.application;
 import com.playlist.plitter.playlist.application.dto.PlaylistCreateResponse;
 import com.playlist.plitter.playlist.domain.entity.PlaylistEntity;
 import com.playlist.plitter.playlist.domain.repository.PlaylistRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.playlist.plitter.auth.domain.entity.UserEntity;
+import com.playlist.plitter.auth.domain.repository.UserRepository;
+import com.playlist.plitter.global.exception.ApiException;
+import com.playlist.plitter.playlist.exception.PlaylistErrorCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class PlaylistService {
 
-    @Autowired
-    PlaylistRepository playlistRepository;
+    private final PlaylistRepository playlistRepository;
+    private final UserRepository userRepository;
 
-    public PlaylistCreateResponse savePlaylist(PlaylistEntity playlist) {
+    public PlaylistCreateResponse savePlaylist(Long userId) {
+
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("유저 없음"));
+
+        if (playlistRepository.existsByOwner(user)) {
+            throw new ApiException(PlaylistErrorCode.PLAYLIST_ALREADY_EXISTS);
+        }
+
+        String shortId = UUID.randomUUID().toString().substring(0, 5);
+
+        PlaylistEntity playlist = PlaylistEntity.builder()
+                .owner(user)
+                .shortId(shortId)
+                .characterVersion(0)
+                .build();
 
         PlaylistEntity saved = playlistRepository.save(playlist);
 
         Long playlistId = saved.getId();
-        String shortId = UUID.randomUUID().toString().substring(0, 5);
         String shareUrl = "https://ourdomain.com/playlist/" + shortId;
 
         return new PlaylistCreateResponse(playlistId, shortId, shareUrl);
