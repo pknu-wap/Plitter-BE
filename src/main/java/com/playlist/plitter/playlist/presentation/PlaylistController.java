00bd351a73f765caa55d14a17104a3b94233a279
MinseoKim01 <202513341@pukyong.ac.kr>
2026-04-12 11:17:05 +0900

    refactor: 리뷰반영 - Playlist 도메인 예외 처리 구조 수정
    - 전역 예외 처리 방식으로 변경
    - 로그 확인 print문 삭제

--- src/main/java/com/playlist/plitter/playlist/presentation/PlaylistController.java
+++ src/main/java/com/playlist/plitter/playlist/presentation/PlaylistController.java
@@ -4,19 +4,17 @@ import com.playlist.plitter.auth.domain.entity.UserEntity;
 import com.playlist.plitter.auth.domain.repository.UserRepository;
 import com.playlist.plitter.global.dto.ResponseDto;
 import com.playlist.plitter.global.dto.SuccessMessage;
+import com.playlist.plitter.global.exception.ApiException;
 import com.playlist.plitter.playlist.application.PlaylistService;
 import com.playlist.plitter.playlist.application.dto.PlaylistCreateResponse;
 import com.playlist.plitter.playlist.domain.entity.PlaylistEntity;
 import com.playlist.plitter.playlist.domain.repository.PlaylistRepository;
+import com.playlist.plitter.playlist.exception.PlaylistErrorCode;
 import lombok.RequiredArgsConstructor;
-import org.h2.engine.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.http.ResponseEntity;
 
 import java.util.UUID;
 
@@ -36,13 +34,12 @@ public class PlaylistController {
         Long userId = 1L; // 🧐 TODO: JWT 토큰에서 userId 추출
 
         System.out.println("user id = " + userId);
-        System.out.println("✅ playlist 생성 컨트롤러 시작");
 
         UserEntity user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("유저 없음"));
 
         if (playlistRepository.existsByOwner(user)) {
-            throw new PlaylistAlreadyExistsException();
+            throw new ApiException(PlaylistErrorCode.PLAYLIST_ALREADY_EXISTS);
         }
 
         String shortId = UUID.randomUUID().toString().substring(0, 8);
@@ -55,19 +52,6 @@ public class PlaylistController {
 
 
         PlaylistCreateResponse response = playlistService.savePlaylist(playlist);
-        System.out.println("playlist 생성 컨트롤러 종료");
-        System.out.println("============================================");
         return ResponseDto.ofSuccess(SuccessMessage.OPERATION_SUCCESS, response);
-    }
-
-    @ExceptionHandler(PlaylistAlreadyExistsException.class)
-    public ResponseEntity<ResponseDto<Void>> handlePlaylistAlreadyExists() {
-        System.out.println("PLAYLIST_ALREADY_EXISTS 예외 처리되어 종료");
-        System.out.println("============================================");
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(ResponseDto.ofFailure("PLAYLIST_ALREADY_EXISTS", "플레이리스트가 이미 존재합니다."));
-    }
-
-    static class PlaylistAlreadyExistsException extends RuntimeException {
     }
 }
