56f56bbf3aea8cbb28b3c310ad4dae1d64643a96
isjuny0 <isjuny0k@gmail.com>
2026-04-22 17:33:00 +0900

    feat: character 도메인 추가

---
+++ src/main/java/com/playlist/plitter/character/domain/entity/CharacterEntity.java
@@ -0,0 +1,48 @@
+package com.playlist.plitter.character.domain.entity;
+
+import com.playlist.plitter.playlist.domain.entity.PlaylistEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "characters")
+public class CharacterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "playlist_id", nullable = false, unique = true)
+    private PlaylistEntity playlist;
+
+    @Column(name = "version", nullable = false)
+    private int version;
+
+    @Column(name = "image_url", nullable = false, columnDefinition = "TEXT")
+    private String imageUrl;
+
+    @Column(name = "prompt_text", columnDefinition = "TEXT")
+    private String promptText;
+
+    @Column(name = "feature_summary_json", columnDefinition = "jsonb")
+    private String featureSummaryJson;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
