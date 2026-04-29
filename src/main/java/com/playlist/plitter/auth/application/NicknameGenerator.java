085b9d04d786cec0a0fa5592023f3a8bece50b27
yell252 <yellthehistory@gmail.com>
2026-04-29 17:31:08 +0900

    fix:NicknameGenerator 방식 변경

--- src/main/java/com/playlist/plitter/auth/application/NicknameGenerator.java
+++ src/main/java/com/playlist/plitter/auth/application/NicknameGenerator.java
@@ -1,4 +1,4 @@
-package com.playlist.plitter.auth.application;
+import com.playlist.plitter.auth.domain.repository.GuestUserRepository;
 
 import java.util.Random;
 
@@ -7,7 +7,6 @@ public class NicknameGenerator {
     private static final String[] ADJECTIVES = {
             "행복한", "사랑스러운", "아름다운", "사나운", "차가운", "그리운",
             "엄청난", "거대한", "슬픈", "건강한", "멋진"
-
     };
 
     private static final String[] NOUNS = {
@@ -17,10 +16,22 @@ public class NicknameGenerator {
 
     private static final Random RANDOM = new Random();
 
-    public static String generate() {
+    public static String generate(GuestUserRepository guestUserRepository) {
         String adj = ADJECTIVES[RANDOM.nextInt(ADJECTIVES.length)];
         String noun = NOUNS[RANDOM.nextInt(NOUNS.length)];
-        int number = RANDOM.nextInt(9000) + 1000;
-        return adj + noun + "_" + number;
+        String base = adj + noun;
+
+        if (!guestUserRepository.existsByRandomNicknameStartingWith(base)) {
+            return base;
+        }
+
+        int number = 1;
+        String nickname;
+        do {
+            nickname = base + "_" + number;
+            number++;
+        } while (guestUserRepository.existsByRandomNickname(nickname));
+
+        return nickname;
     }
 }
