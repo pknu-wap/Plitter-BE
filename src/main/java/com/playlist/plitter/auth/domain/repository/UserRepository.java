3d971fe0a925b20ab3b83fb335d500680a812b8a
yell252 <yellthehistory@gmail.com>
2026-05-04 19:49:28 +0900

    fix: userrepository가 잘못된 위치에 있어 삭제

--- src/main/java/com/playlist/plitter/auth/domain/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.playlist.plitter.auth.domain.repository;
-
-import com.playlist.plitter.auth.domain.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-    public interface UserRepository extends JpaRepository<UserEntity, Long> {
-        boolean existsByGuestToken(String guestToken);
-    }
-
