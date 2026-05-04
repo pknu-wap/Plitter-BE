25da10fb8a728fc8e3d144cdce98bd4fb5538fde
yell252 <yellthehistory@gmail.com>
2026-05-04 20:03:18 +0900

    fix: merge를 위한 entity와 guestEntity 분리

--- src/main/java/com/playlist/plitter/auth/domain/entity/UserEntity.java
+++ src/main/java/com/playlist/plitter/auth/domain/entity/UserEntity.java
@@ -5,42 +5,39 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "users")
 @Getter
-@Table(name = "guest_sessions")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
-
-
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "guest_token", nullable = false, unique = true)
-    private String guestToken;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 
-    @Column(name = "random_nickname", nullable = false)
-    private String randomNickname;
-
-    @CreatedDate
-    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @LastModifiedDate
+    @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
     @Builder
-    public UserEntity(String guestToken, String randomNickname) {
-        this.guestToken = guestToken;
-        this.randomNickname = randomNickname;
+    public UserEntity(Role role) {
+        this.role = role;
+    }
+
+    public enum Role {
+        USER, ADMIN
     }
 }
