e6a451409906f6d3f7bc30fae205c892f5fa8192
yell252 <yellthehistory@gmail.com>
2026-05-04 19:47:59 +0900

    fix: 머지를 위한 파일명 변경

---
+++ src/main/java/com/playlist/plitter/auth/domain/entity/UserEntity.java
@@ -0,0 +1,46 @@
+package com.playlist.plitter.auth.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "guest_sessions")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+
+
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "guest_token", nullable = false, unique = true)
+    private String guestToken;
+
+    @Column(name = "random_nickname", nullable = false)
+    private String randomNickname;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public UserEntity(String guestToken, String randomNickname) {
+        this.guestToken = guestToken;
+        this.randomNickname = randomNickname;
+    }
+}
