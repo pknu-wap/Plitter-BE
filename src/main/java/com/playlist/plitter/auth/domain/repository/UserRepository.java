249e6c3480b95c149b4b64ccb69aec199a4b3444
yell252 <yellthehistory@gmail.com>
2026-05-04 19:44:35 +0900

    fix: merge 하는데 필요없는 파일 삭제

--- src/main/java/com/playlist/plitter/auth/domain/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.playlist.plitter.auth.domain.repository;
-
-import com.playlist.plitter.auth.domain.entity.GuestUserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-    public interface UserRepository extends JpaRepository<GuestUserEntity, Long> {
-        boolean existsByGuestToken(String guestToken);
-    }
-
