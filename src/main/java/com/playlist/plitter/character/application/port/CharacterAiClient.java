95ffdecdca7c0006d6fc7c62b636908a2e051018
isjuny0 <isjuny0k@gmail.com>
2026-05-19 22:19:43 +0900

    refactor(character): 미사용 AI 스펙 포트/스텁 제거

--- src/main/java/com/playlist/plitter/character/application/port/CharacterAiClient.java
+++
@@ -1,8 +0,0 @@
-package com.playlist.plitter.character.application.port;
-
-import com.playlist.plitter.character.application.port.dto.CharacterEditSpec;
-import com.playlist.plitter.character.application.port.dto.CharacterEditSpecRequest;
-
-public interface CharacterAiClient {
-    CharacterEditSpec createEditSpec(CharacterEditSpecRequest request);
-}
