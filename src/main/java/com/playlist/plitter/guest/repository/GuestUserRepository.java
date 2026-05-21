4ae7663b74bc388c7e75fbcc8bb5bfe14c64a6c5
MinseoKim01 <202513341@pukyong.ac.kr>
2026-05-21 12:18:30 +0900

    fix: 게스트 추천 시 세션 랜덤 닉네임 저장
    - guestToken으로 게스트 세션을 조회하도록 수정
    - 추천 등록 시 요청값이 아닌 guest_sessions의 randomNickname을 사용
    - 유효하지 않은 게스트 토큰에 대한 GUEST_NOT_FOUND 에러 추가

--- src/main/java/com/playlist/plitter/guest/repository/GuestUserRepository.java
+++ src/main/java/com/playlist/plitter/guest/repository/GuestUserRepository.java
@@ -3,8 +3,11 @@ package com.playlist.plitter.guest.repository;
 import com.playlist.plitter.guest.entity.GuestUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GuestUserRepository extends JpaRepository<GuestUserEntity, Long> {
     boolean existsByGuestToken(String guestToken);
+    Optional<GuestUserEntity> findByGuestToken(String guestToken);
     boolean existsByRandomNickname(String randomNickname);
     boolean existsByRandomNicknameStartingWith(String prefix);
 }
