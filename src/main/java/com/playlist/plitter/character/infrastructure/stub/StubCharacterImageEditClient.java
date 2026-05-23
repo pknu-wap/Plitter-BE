da38504ac3eeb77a9a3dd8f2c79ab9267ca04e7f
isjuny0 <isjuny0k@gmail.com>
2026-05-23 16:41:51 +0900

    refactor(character): 스텁 이미지 편집기 프로파일 분기 적용 (#47)

--- src/main/java/com/playlist/plitter/character/infrastructure/stub/StubCharacterImageEditClient.java
+++ src/main/java/com/playlist/plitter/character/infrastructure/stub/StubCharacterImageEditClient.java
@@ -2,8 +2,10 @@ package com.playlist.plitter.character.infrastructure.stub;
 
 import com.playlist.plitter.character.application.port.CharacterImageEditClient;
 import com.playlist.plitter.character.application.port.dto.CharacterImageEditRequest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("character-stub")
 @Component
 public class StubCharacterImageEditClient implements CharacterImageEditClient {
 
