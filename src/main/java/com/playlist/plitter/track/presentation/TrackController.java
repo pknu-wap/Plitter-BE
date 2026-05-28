4bc83b5da917eb423ef5d6b9d1be9651509c646a
JunYoung <june3610@office.pknu.ac.kr>
2026-05-28 15:31:38 +0900

    feat: 노래 재생 API 엔드포인트

--- src/main/java/com/playlist/plitter/track/presentation/TrackController.java
+++ src/main/java/com/playlist/plitter/track/presentation/TrackController.java
@@ -3,12 +3,10 @@ package com.playlist.plitter.track.presentation;
 import com.playlist.plitter.global.dto.ResponseDto;
 import com.playlist.plitter.global.dto.SuccessMessage;
 import com.playlist.plitter.track.application.TrackService;
+import com.playlist.plitter.track.application.dto.TrackPlayResponse;
 import com.playlist.plitter.track.application.dto.TrackSearchResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +23,14 @@ public class TrackController {
             @RequestParam(defaultValue = "10") Integer limit
     ) {
         List<TrackSearchResponse> result = trackService.searchTracks(keyword, limit);
+        return ResponseDto.ofSuccess(SuccessMessage.OPERATION_SUCCESS, result);
+    }
+
+    @GetMapping("/{spotifyTrackId}/play")
+    public ResponseDto<TrackPlayResponse> getTrackEmbedUrl(
+            @PathVariable String spotifyTrackId
+    ) {
+        TrackPlayResponse result = trackService.getTrackEmbedUrl(spotifyTrackId);
         return ResponseDto.ofSuccess(SuccessMessage.OPERATION_SUCCESS, result);
     }
 }
