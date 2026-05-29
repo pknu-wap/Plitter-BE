58787c0db8a04126a399ef0075b8b22521232c93
isjuny0 <isjuny0k@gmail.com>
2026-05-29 23:28:32 +0900

    refactor(character): 피처 집계 샘플 수를 응답에 포함

--- src/main/java/com/playlist/plitter/character/infrastructure/RecommendationMusicFeatureClient.java
+++ src/main/java/com/playlist/plitter/character/infrastructure/RecommendationMusicFeatureClient.java
@@ -115,6 +115,10 @@ public class RecommendationMusicFeatureClient implements MusicFeatureClient {
         payload.put("avgBpm", average(summary.bpmSum, summary.bpmCount));
         payload.put("avgEnergy", average(summary.energySum, summary.energyCount));
         payload.put("avgValence", average(summary.valenceSum, summary.valenceCount));
+        payload.put("bpmCount", summary.bpmCount);
+        payload.put("energyCount", summary.energyCount);
+        payload.put("valenceCount", summary.valenceCount);
+        payload.put("genreCount", summary.genreCounts.values().stream().mapToInt(Integer::intValue).sum());
         payload.put("primaryGenre", findPrimaryGenre(summary.genreCounts));
         payload.put("genreDistribution", summary.genreCounts);
 
