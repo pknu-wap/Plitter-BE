76d50383e502811255be904ca8ae48fa4715faf9
isjuny0 <isjuny0k@gmail.com>
2026-06-01 00:25:58 +0900

    feat(playlist): public 응답에 ownerNickname/recommendations 추가 (#74)

--- src/main/java/com/playlist/plitter/playlist/application/dto/PlaylistPublicResponse.java
+++ src/main/java/com/playlist/plitter/playlist/application/dto/PlaylistPublicResponse.java
@@ -1,10 +1,16 @@
 package com.playlist.plitter.playlist.application.dto;
 
+import com.playlist.plitter.recommendations.application.dto.RecommendationResponse;
+
+import java.util.List;
+
 public record PlaylistPublicResponse(
         Long playlistId,
         String shortId,
         Integer recommendationCount,
         Boolean canCreateCharacter,
+        String ownerNickname,
+        List<RecommendationResponse> recommendations,
         String latestCoverImageUrl
 ) {
 }
